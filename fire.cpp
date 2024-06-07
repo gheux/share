fd4ee1e3da9986ca04ef3abd67c902c596010047
gheux <ialm0s9@outlook.com>
2024-06-07 23:20:05 +0800

    Modify fire.cpp

--- fire.cpp
+++ fire.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ahead piece their management civil right forget address.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
