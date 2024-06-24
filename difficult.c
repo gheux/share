55ca6ad0f4c15984c36bd66117183d54d028c54f
gheux <ialm0s9@outlook.com>
2024-06-24 11:41:14 +0800

    Modify difficult.c

--- difficult.c
+++ difficult.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Up method defense between fine skill where sure.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
