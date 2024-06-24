e2757ba1727423f5e88348e70ea83e8767ec8b8d
gheux <ialm0s9@outlook.com>
2024-06-24 11:41:14 +0800

    Modify others.cpp

--- others.cpp
+++ others.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Break skill ever.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
