0f2166734ca4bf2e4b0147594796f79be0b544be
gheux <ialm0s9@outlook.com>
2024-06-27 10:53:04 +0800

    Modify ball.c

--- ball.c
+++ ball.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# College party together consider especially street how.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
