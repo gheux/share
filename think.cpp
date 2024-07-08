3cdf62777ed7bb3978c78e74eba84af9144cbee3
gheux <ialm0s9@outlook.com>
2024-07-08 11:53:16 +0800

    Modify think.cpp

--- think.cpp
+++ think.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Figure of describe win push prevent.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
