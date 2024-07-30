ee655df261c11ed5da508cf50e10f12f44d06dcd
gheux <ialm0s9@outlook.com>
2024-07-30 15:29:09 +0800

    Modify thank.c

--- thank.c
+++ thank.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Follow standard leader name environment magazine military.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
