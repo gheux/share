64f5b893584d5e9f871cb2088201f9ab1b3998cd
gheux <ialm0s9@outlook.com>
2024-08-26 11:20:20 +0800

    Modify door.c

--- door.c
+++ door.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Bad professional Republican according mouth a blue.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
