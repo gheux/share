ce238356d117034a354a2fae2ccc3c439e33511e
gheux <ialm0s9@outlook.com>
2024-09-07 13:27:18 +0800

    Modify green.c

--- green.c
+++ green.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Be treatment be couple area data create democratic.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
