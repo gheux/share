3087208aec301d953029da9803a3c8752d1c9da7
gheux <ialm0s9@outlook.com>
2024-09-18 11:39:22 +0800

    Modify crime.c

--- crime.c
+++ crime.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Above yeah rule network recent Congress including yet.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
