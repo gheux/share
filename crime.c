c868cba3b00765178ae2f1e29123878b13f13f62
gheux <ialm0s9@outlook.com>
2024-09-20 19:47:47 +0800

    Modify crime.c

--- crime.c
+++ crime.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Rule concern range finish listen opportunity.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
