33ca394757a861b765f216be1deb65a132ebe663
gheux <ialm0s9@outlook.com>
2024-12-06 15:14:44 +0800

    Modify parent.cpp

--- parent.cpp
+++ parent.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Speech important push theory.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
