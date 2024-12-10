78be110f7fadabf69f615cb9bbafde0d489d0e6f
gheux <ialm0s9@outlook.com>
2024-12-11 06:34:52 +0800

    Modify civil.c

--- civil.c
+++ civil.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Interview agree item many account laugh occur.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
