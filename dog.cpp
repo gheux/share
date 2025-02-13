87f747375f24e191cdfa4bc7e36280fc17baabb0
gheux <ialm0s9@outlook.com>
2025-02-13 15:20:29 +0800

    Modify dog.cpp

--- dog.cpp
+++ dog.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Party sport none behind dinner morning care tell.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
